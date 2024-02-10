a7df96ad2d8668a4099e086d1892266492d4d707
Sadhurahavan <109868957+Sadhurahavan5555@users.noreply.github.com>
2024-02-10 14:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,12 @@ with st.sidebar:
 if selected == "Notes":
     st.markdown(
             """
-    ▸ By utilizing the OpenAI API key, I can help you take advantage of cutting-edge natural language processing technology.
-    This will allow your application to analyze and generate text with remarkable accuracy and speed. The OpenAI API key provides access
-    to a wide range of AI-powered language models that can be trained to perform various tasks such as language translation, sentiment analysis, and question-answering. 
-    In addition to text analysis, I can also implement TTS (Text-to-Speech) functionality in your application. This feature will enable your users to 
-    listen to the generated text in a more natural and human-like voice. TTS technology uses advanced algorithms to convert written text into spoken words, 
-    which can be particularly useful for people with visual impairments or reading difficulties.
-    Finally, I can create a user-friendly GUI (Graphical User Interface) that will make it easy for your users to interact with your application and access its features. 
-    The GUI can be designed to match your brand's look and feel and can include various widgets such as buttons, menus, and forms. This will provide your users with a seamless and intuitive experience.
+    ▸ Hello there! I'm excited to let you know that this website was created using OpenAI API key, Python, Streamlit, and other required libraries. These tools were used to design a 
+    user-friendly interface that provides a seamless experience for you. 
+    ▸ If you're interested in exploring further, you can check out my GitHub where you'll find more information about the development process 
+     and the different features of the website. There, you'll also find instructions on how you can modify the website for your own usage. 
+    ▸ I hope you find this website helpful and informative. If you have any questions or doubts about the website,
+    please don't hesitate to reach out. I'd love to hear your feedback and suggestions as well. Thank you for visiting!
     """) 
 
     st.markdown("Check out my github:")
